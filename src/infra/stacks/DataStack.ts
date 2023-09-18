81687a9d0e80aef5b43ef0a8eb4af6572ce4b1a4
ge0vx <geovapb@gmail.com>
2023-09-18 08:33:37 -0700

    define dynamodb table

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,9 +1,20 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, ITable, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 export class DataStack extends Stack {
 
+    public readonly favPokemonTable: ITable;
+
     constructor(scope: Construct, id: string, props?: StackProps){
         super(scope, id, props)
+
+        this.favPokemonTable = new Table(this, 'favPokemonTable', {
+            partitionKey : {
+                name: 'id',
+                type: AttributeType.STRING
+            },
+            tableName : 'favPokemonTable'
+        })
     }
 }
