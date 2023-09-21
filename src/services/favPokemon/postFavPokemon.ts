13b8fa56a5223162325f5c51235c5a376406fcdb
ge0vx <geovapb@gmail.com>
2023-09-21 06:08:23 -0700

    add validation in post service

--- src/services/favPokemon/postFavPokemon.ts
+++ src/services/favPokemon/postFavPokemon.ts
@@ -1,7 +1,7 @@
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, PutItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { v4 } from "uuid";
-import { marshall } from "@aws-sdk/util-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 
 export async function postFavPokemon(event:APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
     
@@ -12,6 +12,31 @@ export async function postFavPokemon(event:APIGatewayProxyEvent, ddbClient: Dyna
     item.types = item.types;
     item.user_id = item.user_id;
     item.id = v4();
+
+    if(!item.name || !item.user_id){
+        return {
+            statusCode: 200,
+            body: JSON.stringify(`Pokemon name and user is required!`)
+        }
+    }else{
+
+        const getItemResponse = await ddbClient.send(new ScanCommand({
+            TableName: process.env.TABLE_NAME,
+            FilterExpression: "user_id = :user_id",
+            ExpressionAttributeValues: {
+                ":user_id": { S: item.user_id }
+            },
+        }))
+
+        const unmashalledItems = getItemResponse.Items?.map(item => unmarshall(item));
+        const filterData = unmashalledItems.filter((i)=>i.name===item.name)
+        if(filterData.length>0){
+            return {
+                statusCode: 400,
+                body: JSON.stringify(`Pokemon ${item.name} added for this user`)
+            }
+        }
+    }
 
     await ddbClient.send(new PutItemCommand({
         TableName: process.env.TABLE_NAME,
