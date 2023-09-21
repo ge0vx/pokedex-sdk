9947ff5c379d774f14b315351bb7c67d084b0c98
ge0vx <geovapb@gmail.com>
2023-09-21 06:09:35 -0700

    get fav pokemons by user

--- src/services/favPokemon/getFavPokemon.ts
+++ src/services/favPokemon/getFavPokemon.ts
@@ -4,13 +4,47 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 export async function getFavPokemon(event: APIGatewayProxyEvent, ddbClient: DynamoDBClient): Promise<APIGatewayProxyResult> {
 
+    if(event.queryStringParameters) {
+        if ('user_id' in event.queryStringParameters) {
 
+            const user_id = event.queryStringParameters['user_id'];
+            
+            const getItemResponse = await ddbClient.send(new ScanCommand({
+                TableName: process.env.TABLE_NAME,
+                FilterExpression: "user_id = :user_id",
+                ExpressionAttributeValues: {
+                    ":user_id": { S: user_id }
+                },
+            }))
+            
+            if (getItemResponse) {
+                const unmashalledItems = getItemResponse.Items?.map(item => unmarshall(item));
+
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({results: unmashalledItems})
+                }
+            } else {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify(`Pokemons with user not found!`)
+                }
+            }
+
+        } else {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('User Id required!')
+            }
+        }
+    }
+    
     const result = await ddbClient.send(new ScanCommand({
         TableName: process.env.TABLE_NAME,
     }));
-    
-    const unmashalledItems = result.Items?.map(item => unmarshall(item));
 
+    const unmashalledItems = result.Items?.map(item => unmarshall(item));
+    
     return {
         statusCode: 200,
         body: JSON.stringify(unmashalledItems)
