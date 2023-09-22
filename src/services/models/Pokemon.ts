fe20c728a14ddac7273fd557a3219b2b9b540fe3
ge0vx <geovapb@gmail.com>
2023-09-22 07:09:41 -0700

    get method handler for external pokemon api get pokemon by name

---
+++ src/services/models/Pokemon.ts
@@ -0,0 +1,5 @@
+export interface Pokemon {
+    pokemon_id: number,
+    name: string,
+    default_sprite: string
+}
--- src/services/pokemon/handler.ts
+++ src/services/pokemon/handler.ts
@@ -1,14 +1,46 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { PokemonClient } from 'pokenode-ts';
+import { Pokemon } from "../models/Pokemon";
 
 
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
     const api = new PokemonClient();
+    
+    if(event.queryStringParameters) {
+        if ('pokemon_name' in event.queryStringParameters) {
+            const pokemon = event.queryStringParameters['pokemon_name'];
+            try {
+                const getItemResponse = await api.getPokemonByName(pokemon);
+
+                if (getItemResponse) {
+                    const pokemon:Pokemon = {
+                        pokemon_id: getItemResponse.id,
+                        name: getItemResponse.name,
+                        default_sprite: getItemResponse?.sprites?.front_default,
+                    }
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify({results:[pokemon]})
+                    }
+                } else {
+                    return {
+                        statusCode: 400,
+                        body: JSON.stringify(`${pokemon} not found!`)
+                    }
+                }
+        
+            } catch (error) {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify("error")
+                }
+            }
+        }
+    }
 
     try {
         const res = await api.listPokemons();
-        console.log(res);
 
         return {
             statusCode: 200,
